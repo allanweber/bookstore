d7c161df0f41d0ac89ece22fe0e691d76ca9d33f
allanweber <allanweber@outlook.com>
2018-03-10 18:37:38 -0300

    Ajsustes

--- Bookstore.Web/src/app/livros/livros.component.ts
+++ Bookstore.Web/src/app/livros/livros.component.ts
@@ -12,7 +12,6 @@ import { MessageService } from '../core/message.service';
 export class LivrosComponent implements OnInit, OnDestroy {
   public livros: Livro[];
   private serviceInscription: Subscription;
-  public isDeleting: boolean;
   constructor(private service: LivroService) {}
 
   ngOnInit() {
@@ -30,7 +29,6 @@ export class LivrosComponent implements OnInit, OnDestroy {
   }
 
   delete(livro: Livro) {
-    this.isDeleting = true;
     MessageService.Confirm(
       `Tem certeza que deseja remover o livro ${livro.title}?`
     ).then(result => {
@@ -39,11 +37,8 @@ export class LivrosComponent implements OnInit, OnDestroy {
           MessageService.SuccessToaster(
             `Livro ${livro.title} removido com sucesso.`
           );
-          this.isDeleting = false;
           this.load();
-        }, error => (this.isDeleting = false));
-      } else {
-        this.isDeleting = false;
+        });
       }
     });
   }
